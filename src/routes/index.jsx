5662c102bc1b290aef3eab18b6e43d55cdeae378
arslan0510 <arslanjutt23@gmail.com>
2021-01-18 18:38:07 +0500

    changes applied to dashboard

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Route, BrowserRouter, Switch, Redirect, Link } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import { history } from "../helpers";
+import {Route, BrowserRouter, Switch, Link} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
 import {
   AddDeveloper,
   AddFeedback,
@@ -12,13 +11,11 @@ import {
   ProjectDetails,
   TechStack,
 } from "./app-flow";
-import Login from "./auth/Login";
-import Register from "./auth/Register";
-import { routes } from "../services";
-import { Sidebar } from "../components";
-import { Button } from "react-bootstrap";
+import {Login, Register} from "./auth";
+import {routes} from "../services";
+import {Sidebar} from "../components";
 
-const index = ({ isAuthorized }) => {
+const index = ({isAuthorized}) => {
   return (
     <>
       <ToastContainer
@@ -35,30 +32,39 @@ const index = ({ isAuthorized }) => {
       <BrowserRouter>
         {!isAuthorized ? (
           <Switch>
-            <Route exact path="/" component={Login} />
+            <Route exact path='/' component={Login} />
             <Route exact path={routes.login} component={Login} />
             <Route exact path={routes.register} component={Register} />
-            <Route path="*" component={NotFound} />
+            <Route path='*' component={NotFound} />
           </Switch>
-        )
-          :
-          (
-            <>
-              <Sidebar />
-              <Switch>
-                <Route exact path={routes.dashboard} component={Dashboard} />
-                <Route exact path={routes.feedback} component={AddFeedback} />
-                <Route exact path={routes.cFeedback} component={CompletedFeedback} />
-                <Route exact path={routes.projectDetails} component={ProjectDetails} />
-                <Route exact path={routes.projects} component={Projects} />
-                <Route exact path={routes.addDeveloper} component={AddDeveloper} />
-                <Route exact path={routes.developers} component={Developers} />
-                <Route exact path={routes.techStack} component={TechStack} />
-                <Route path="*" component={Dashboard} />
-              </Switch>
-            </>
-          )
-        }
+        ) : (
+          <>
+            <Sidebar />
+            <Switch>
+              <Route exact path={routes.dashboard} component={Dashboard} />
+              <Route exact path={routes.feedback} component={AddFeedback} />
+              <Route
+                exact
+                path={routes.cFeedback}
+                component={CompletedFeedback}
+              />
+              <Route
+                exact
+                path={routes.projectDetails}
+                component={ProjectDetails}
+              />
+              <Route exact path={routes.projects} component={Projects} />
+              <Route
+                exact
+                path={routes.addDeveloper}
+                component={AddDeveloper}
+              />
+              <Route exact path={routes.developers} component={Developers} />
+              <Route exact path={routes.techStack} component={TechStack} />
+              <Route path='*' component={Dashboard} />
+            </Switch>
+          </>
+        )}
       </BrowserRouter>
     </>
   );
@@ -67,18 +73,12 @@ const index = ({ isAuthorized }) => {
 export default index;
 
 export const NotFound = () => (
-  <div className="w-100 h-100 d-flex flex-column p-0 m-0 justify-content-center align-items-center">
-    <h1 className="text-secondary">
-      404!
-    </h1>
+  <div className='w-100 h-100 d-flex flex-column p-0 m-0 justify-content-center align-items-center'>
+    <h1 className='text-secondary'>404!</h1>
     <br />
-    <p>
-      The page you are looking for is not found
-    </p>
-    <Link to={routes.login} style={{ textDecoration: "none" }}>
-      <div className="btn-light px-4 py-2 rounded">
-        Go to login
-    </div>
+    <p>The page you are looking for is not found</p>
+    <Link to={routes.login} style={{textDecoration: "none"}}>
+      <div className='btn-light px-4 py-2 rounded'>Go to login</div>
     </Link>
   </div>
-)
+);
